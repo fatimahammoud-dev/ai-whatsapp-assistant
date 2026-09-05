c795af18c0f144953214bb8f17b52292cc3aa410
issahammoud <issa.hammoud@outlook.com>
2026-09-05 12:01:56 +0200

    black reformatting

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -7,14 +7,14 @@ class TenantScopedMixin:
     def get_queryset(self):
         queryset = super().get_queryset()
         user = self.request.user
-    
+
         if not user.is_authenticated:
             return queryset.none()
-    
+
         if user.is_superuser or user.role == StaffUser.Role.PLATFORM_ADMIN:
             return queryset
-    
+
         if not user.tenant_id:
             return queryset.none()
-    
+
         return queryset.filter(**{self.tenant_lookup: user.tenant_id})
